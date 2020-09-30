81e0cb11a8ad3d7bb306da8f4608e9b1d005e021
Karan Ahuja <karanahuja5598@gmail.com>
2020-09-30 18:15:36 -0500

    Starting to work on the project

---
+++ src/test/java/com/example/espnscraper/EspnscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.espnscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspnscraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
